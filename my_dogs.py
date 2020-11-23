c2f385e10414cbcb982591259919dc6aeb225da2
Gobind Puniani <gobindpuniani@gmail.com>
2020-11-22 16:36:13 -0800

    Finished first page of tutorial

---
+++ my_dogs.py
@@ -0,0 +1,16 @@
+# my_dogs.py
+from dog import Dog
+
+my_dog = Dog("Rex", "SuperDog")
+my_dog.bark()
+
+my_other_dog = Dog("Annie", "SuperDog")
+print(my_other_dog.name)
+
+new_dog1 = Dog("Thor", "Labrador")
+new_dog2 = Dog("Drax", "German Shepherd")
+new_dog3 = Dog("Rocket", "Retriever")
+
+new_dog1.bark()
+new_dog2.sit()
+new_dog3.roll_over()
