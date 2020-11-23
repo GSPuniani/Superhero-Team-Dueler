15a068a87440bf4c749dfd435797d91f078ef2f5
Gobind Puniani <gobindpuniani@gmail.com>
2020-11-22 22:05:58 -0800

    Created add_ability method

--- hero.py
+++ hero.py
@@ -1,3 +1,5 @@
+from ability import Ability
+from armor import Armor
 import random
 
 class Hero:
@@ -5,10 +7,17 @@ class Hero:
     # so we can set that in the function header.
     def __init__(self, name, starting_health=100):
         '''Instance properties:
-          name: String
-          starting_health: Integer
-          current_health: Integer
+            abilities: List
+            armors: List
+            name: String
+            starting_health: Integer
+            current_health: Integer
         '''
+
+        # abilities and armors don't have starting values,
+        # and are set to empty lists on initialization
+        self.abilities = list()
+        self.armors = list()
         # we know the name of our hero, so we assign it here
         self.name = name
         # similarly, our starting health is passed in, just like name
@@ -16,6 +25,7 @@ class Hero:
         # when a hero is created, their current health is
         # always the same as their starting health (no damage taken yet!)
         self.current_health = starting_health
+
 
     def fight(self, opponent):
         ''' Current Hero will take turns fighting the opponent hero passed in.
@@ -25,10 +35,27 @@ class Hero:
         print(f"{winner.name} won!")
 
 
+    def add_ability(self, ability):
+        ''' Add ability to abilities list '''
+
+        # We use the append method to add ability objects to our list.
+        self.abilities.append(ability)
+
+
+
+# if __name__ == "__main__":
+#     # If you run this file from the terminal
+#     # this block is executed.
+#     my_hero = Hero("Grace Hopper", 200)
+#     print(my_hero.name)
+#     print(my_hero.current_health)
+
 if __name__ == "__main__":
     # If you run this file from the terminal
     # this block is executed.
-    hero1 = Hero("Wonder Woman")
-    hero2 = Hero("Dumbledore")
-
-    hero1.fight(hero2)
+    ability = Ability("Great Debugging", 50)
+    hero = Hero("Grace Hopper", 200)
+    hero.add_ability(ability)
+    ability2 = Ability("Better Debugging", 100)
+    hero.add_ability(ability2)
+    print(hero.abilities)
